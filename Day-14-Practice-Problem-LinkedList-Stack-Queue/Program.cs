3fb859d4f86ba92d6439b9d36f12c23f687e4a0f
Ugesh Kumar KS <ugesh262000@gmail.com>
2023-06-20 07:56:48 +0530

    [ ugesh - Welcome note ]

---
+++ Day-14-Practice-Problem-LinkedList-Stack-Queue/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Day_14_Practice_Problem_LinkedList_Stack_Queue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Welcome to LinkedList,Stack & Queue Problem ");
+        }
+    }
+}
